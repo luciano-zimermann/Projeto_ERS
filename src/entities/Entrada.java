e67bf38ff3206c963fa3489969d3e47a817ef207
VandersonJC <confortivanderson@yahoo.com.br>
2021-05-19 21:48:59 -0300

    ProjetoERS versao 1.0

---
+++ src/entities/Entrada.java
@@ -0,0 +1,44 @@
+package entities;
+
+import java.util.Date;
+
+public class Entrada {
+
+    private Integer id_entrada;
+    private Integer unidades;
+    private Date data_entrada;
+    private Produto produto;
+
+    public Integer getId_entrada() {
+        return id_entrada;
+    }
+
+    public void setId_entrada(Integer id_entrada) {
+        this.id_entrada = id_entrada;
+    }
+
+    public Integer getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(Integer unidades) {
+        this.unidades = unidades;
+    }
+
+    public Date getData_entrada() {
+        return data_entrada;
+    }
+
+    public void setData_entrada(Date data_entrada) {
+        this.data_entrada = data_entrada;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+}
